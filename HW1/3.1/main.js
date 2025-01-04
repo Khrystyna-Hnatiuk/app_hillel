608a866eb21e0ff08d812e09114eb93e464a0c85
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-04 10:11:34 +0200

    3.2 added

---
+++ HW1/3.1/main.js
@@ -0,0 +1,28 @@
+let digit =5;
+console.log(typeof digit);
+let str = 'hello';
+console.log(typeof str);
+let bool = true;
+console.log(typeof bool);
+let symb = Symbol('1234')
+console.log(typeof symb);
+let arr = [1,4,6];
+console.log(typeof arr);
+let obj ={
+    age:10,
+    year:2025
+}
+console.log(typeof obj);
+let nulish = null;
+console.log(typeof nulish);
+let undef = undefined;
+console.log(typeof undef);
+
+let bigInteger = 3456128394274898057428498n;
+console.log(typeof bigInteger);
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(typeof sum);
---
+++ HW1/3.2/main.js
@@ -0,0 +1,7 @@
+let str1 = prompt('Enter 1 text');
+let str2 = prompt('Enter 2 text');
+let str3 = prompt('Enter 3 text');
+
+console.log(`text 1 ${str1}`)
+console.log(`text 2 ${str2}`)
+console.log(`text 3 ${str3}`)
