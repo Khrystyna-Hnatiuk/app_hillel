16d9267fb5eb5114319e965001c787a0641db27f
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-08 20:15:50 +0200

    added 4.3

---
+++ hw2/4.3/main.js
@@ -0,0 +1,41 @@
+let birthYear = +prompt("enter year of your birthday");
+let city = prompt("Enter your city");
+let favouriteSport = prompt("Enter favourite type of sport");
+let message = "";
+
+if (birthYear>0) {
+  const age = 2025 - birthYear;
+  message += `Your age : ${age} \n`;
+} else {
+  message += `Unfortunately, you don't want to enter your birthday year\n`;
+}
+
+if (city) {
+  if (city === "Kyiv") {
+    message += `You live in the capital of Ukraine. \n`;
+  } else if (city === "Washington") {
+    message += `You live in the capital of USA. \n`;
+  } else if (city === "London") {
+    message += `You live in the capital of Great Britain. \n`;
+  } else {
+    message += `You live in the ${city} city. \n`;
+  }
+} else {
+  message += `Unfortunately, you don't want to enter your city. \n`;
+}
+
+if (favouriteSport) {
+  if (favouriteSport === "football") {
+    message += `Great! Do you wanna be Cristiano Ronaldo \n? `;
+  } else if (favouriteSport === "athletics") {
+    message += `Great! Do you wanna be Serhiy Bubka? \n`;
+  } else if (favouriteSport === "jumping") {
+    message += `Great! Do you wanna be Yaroslava Mahuchikh?\n`;
+  } else {
+    message += `Your favourite sport is ${favouriteSport} \n`;
+  }
+} else {
+  message += `Unfortunately, you don't want to enter your favourite sport\n`;
+}
+
+alert(message);
---
+++ hw2/4.4/main.js
@@ -0,0 +1,3 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
