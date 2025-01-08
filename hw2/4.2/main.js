fb09c067262a69c328f76d56ad61ddbd65f7d689
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-08 21:30:31 +0200

    done task 4.2

---
+++ hw2/4.2/main.js
@@ -0,0 +1,18 @@
+const digit = +prompt("enter number");
+const splittedDigit = digit.toString().split("");
+if (
+  splittedDigit[0] == splittedDigit[1] &&
+  splittedDigit[1] == splittedDigit[2] &&
+  splittedDigit[0] == splittedDigit[2]
+) {
+  console.log("All numbers are equal");
+} else if (
+  splittedDigit[0] == splittedDigit[1] ||
+  splittedDigit[1] == splittedDigit[2] ||
+  splittedDigit[0] == splittedDigit[2]
+) {
+  console.log("among the number there are numbers that are the same");
+}
+else{
+    console.log(`all numbers are not equal`)
+}
