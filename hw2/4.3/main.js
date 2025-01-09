d92c376e112e2b5069eb34f9aa951bcf3cfea4da
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-09 19:59:27 +0200

    4.3 changed conditions

--- hw2/4.3/main.js
+++ hw2/4.3/main.js
@@ -1,21 +1,23 @@
-const birthYear = +prompt("enter year of your birthday");
+const birthYear =prompt("enter year of your birthday");
 const city = prompt("Enter your city");
 const favouriteSport = prompt("Enter favourite type of sport");
 let message = "";
 
-if (birthYear>0) {
+
+if (birthYear === null || birthYear.trim() === "") {
+  message += `Unfortunately, you don't want to enter your birthday year\n`;
+} else if (birthYear > 1900 && birthYear <= 2025) {
   const age = 2025 - birthYear;
   message += `Your age : ${age} \n`;
 } else {
-  message += `Unfortunately, you don't want to enter your birthday year\n`;
+  message += `Unfortunately, you didn't enter your correct birthday year\n`;
 }
-
 if (city) {
-  if (city === "Kyiv") {
+  if (city.toLowerCase()  === "Kyiv") {
     message += `You live in the capital of Ukraine. \n`;
-  } else if (city === "Washington") {
+  } else if (city.toLowerCase()  === "Washington") {
     message += `You live in the capital of USA. \n`;
-  } else if (city === "London") {
+  } else if (city.toLowerCase()  === "London") {
     message += `You live in the capital of Great Britain. \n`;
   } else {
     message += `You live in the ${city} city. \n`;
@@ -25,11 +27,11 @@ if (city) {
 }
 
 if (favouriteSport) {
-  if (favouriteSport === "football") {
-    message += `Great! Do you wanna be Cristiano Ronaldo \n? `;
-  } else if (favouriteSport === "athletics") {
+  if (favouriteSport.toLowerCase() === "football") {
+    message += `Great! Do you wanna be Cristiano Ronaldo ?\n`;
+  } else if (favouriteSport.toLowerCase()  === "athletics") {
     message += `Great! Do you wanna be Serhiy Bubka? \n`;
-  } else if (favouriteSport === "jumping") {
+  } else if (favouriteSport.toLowerCase()  === "jumping") {
     message += `Great! Do you wanna be Yaroslava Mahuchikh?\n`;
   } else {
     message += `Your favourite sport is ${favouriteSport} \n`;
