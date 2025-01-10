3ce60d3a75a0a17e745b37513836ce4bc8ad0084
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-10 22:12:20 +0200

    4.3 modified

--- hw2/4.3/main.js
+++ hw2/4.3/main.js
@@ -2,25 +2,27 @@ const birthYear =prompt("enter year of your birthday");
 const city = prompt("Enter your city");
 const favouriteSport = prompt("Enter favourite type of sport");
 let message = "";
+const currentYear = new Date().getFullYear();
 
 
 if (birthYear === null || birthYear.trim() === "") {
   message += `Unfortunately, you don't want to enter your birthday year\n`;
-} else if (birthYear > 1900 && birthYear <= 2025) {
-  const age = 2025 - birthYear;
+} else if (birthYear > 1900 && birthYear <=currentYear) {
+  const age = currentYear - birthYear;
   message += `Your age : ${age} \n`;
 } else {
   message += `Unfortunately, you didn't enter your correct birthday year\n`;
 }
 if (city) {
-  if (city.toLowerCase()  === "Kyiv") {
+  const cityToLowerCase = city.toLowerCase();
+  if (cityToLowerCase  === "kyiv") {
     message += `You live in the capital of Ukraine. \n`;
-  } else if (city.toLowerCase()  === "Washington") {
+  } else if (cityToLowerCase  === "washington") {
     message += `You live in the capital of USA. \n`;
-  } else if (city.toLowerCase()  === "London") {
+  } else if (cityToLowerCase  === "london") {
     message += `You live in the capital of Great Britain. \n`;
   } else {
-    message += `You live in the ${city} city. \n`;
+    message += `You live in the ${city} . \n`;
   }
 } else {
   message += `Unfortunately, you don't want to enter your city. \n`;
