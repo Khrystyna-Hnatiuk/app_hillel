9b1b98a07d555b65d12a64aadea98faf5cd52147
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-14 21:19:58 +0200

    loops

--- HW3/5.1/main.js
+++ HW3/5.1/main.js
@@ -1,3 +1,7 @@
-for(let i=20; i<=30; i+=0.5){
-    console.log(i)
+let str = "";
+for (let i = 20; i <= 30; i += 0.5) {
+  str += i + " ";
 }
+
+let strWithComma = str.replace(/\./g,',')
+console.log(strWithComma);
