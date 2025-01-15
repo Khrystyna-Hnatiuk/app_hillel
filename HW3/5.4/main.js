ad80377a2c9fb2028cb1beed12051ad454fa6fe9
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-15 09:59:48 +0200

    5.4

--- HW3/5.4/main.js
+++ HW3/5.4/main.js
@@ -1,37 +1,22 @@
 // Дано ціле число (ввести через 'prompt'). З'ясувати, чи просто воно
 // (простим називається число, більше 1, що не має інших дільників, крім 1 і себе).
-let num = parseFloat(prompt("enter number")); //7
+let input = parseInt(prompt("enter number"));
+let num = parseInt(input, 10);
 //
-if (num > 1) {
+if (isNaN(num) || num <= 1) {
+  console.log("Введіть число більше 1");
+} else {
   let isSimple = true;
 
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
       isSimple = false;
-      break
-    } 
+      break;
+    }
   }
-  if(isSimple){
-    console.log('просте число')
-  }
-  else{
-    console.log('не просте число')
+  if (isSimple) {
+    console.log("просте число");
+  } else {
+    console.log("не просте число");
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
