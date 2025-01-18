65e30d9fd0e2302cf48c66721b7494ebae23f79a
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-18 14:20:54 +0200

    changed 6.1

--- HW4/6.1/main.js
+++ HW4/6.1/main.js
@@ -1,13 +1,19 @@
 // function deleteArrayChar(str, char) {
 //     str = prompt('Enter string');
 //     char = prompt('Enter characters to delete');
+// if(str===''|| char===''){
+//     alert('Please,enter string or characters to delete');
+//     return;
+//  } 
+// let lowStr = str.toLowerCase();
+// let lowChar = char.toLowerCase();
 //   let newStr = "";
-//   for (let elem of str) {
-//     if (!char.includes(elem)) {
+//   for (let elem of lowStr) {
+//     if (!lowChar.includes(elem)) {
 //       newStr += elem;
 //     }
 //   }
-//   return newStr;
+//   return newStr ||'All characters were deleted';
 // }
 
 // console.log(deleteArrayChar());
@@ -15,9 +21,14 @@
 function deleteArrayChar(str, char) {
      str = prompt('Enter string');
      char = prompt('Enter characters to delete');
-  let toArray = str.split("");
-  let result = toArray.filter((elem) => !char.includes(elem)).join("");
-  return result;
-}
+     if(str===''|| char===''){
+        alert('Please,enter string or characters to delete');
+        return;
+     } 
+let lowStr = str.toLowerCase();
+let lowChar = char.toLowerCase();
+      let result = lowStr.split('').filter((elem) => !lowChar.includes(elem)).join("");
+       return result || 'All characters were deleted';    
+     }
 
 console.log(deleteArrayChar())
---
+++ HW4/6.2/main.js
@@ -0,0 +1,10 @@
+let averageSum =(array)=>{
+let newArray =[];
+    for(let elem of arr){
+if(typeof(elem)===Number){
+newArray.push(elem);
+}
+    }
+}
+const arr=['hello',1,7,9,'c','apple',6,false,true];
+console.log(averageSum(arr));
