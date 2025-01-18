c0f89e944222479a4e5223cf9b5392e273fd6d84
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-18 14:48:28 +0200

    6.2

--- HW4/6.2/main.js
+++ HW4/6.2/main.js
@@ -1,10 +1,46 @@
-let averageSum =(array)=>{
-let newArray =[];
-    for(let elem of arr){
-if(typeof(elem)===Number){
-newArray.push(elem);
-}
+function averageSum(arr) {
+    const newarray = arr.filter((item) => typeof item === "number");
+    console.log(newarray);
+    if (newarray.length === 0) {
+      return "0 numeric  in array";
     }
-}
-const arr=['hello',1,7,9,'c','apple',6,false,true];
-console.log(averageSum(arr));
+  
+    const sum = newarray.reduce((accum, current) => accum + current, 0);
+    const average = sum / newarray.length;
+  
+    return average;
+  }
+  const arr1 = ["hello", 1, 7, 9, "c", "apple", 6, false, true, 44, "8"];
+  const arr2 = ["hello", false, true];
+  console.log(averageSum(arr1));
+  console.log(averageSum(arr2));
+  
+
+
+//Стрілкова функція
+// let averageSum = (array) => {
+//   const newArray = [];
+//   let sum = 0;
+
+//   for (let elem of array) {
+//     if (typeof elem === "number") {
+//       newArray.push(elem);
+//       sum += elem;
+//     }
+//   }
+//   if (newArray.length === 0) {
+//     return "0 numeric elements in array";
+//   }
+//   return sum / newArray.length;
+// };
+// const arr1 = ["hello", 1, 7, 9, "c", "apple", 6, false, true, 44, "8"];
+// const arr2 = ["hello", false, true];
+// console.log(averageSum(arr1));
+// console.log(averageSum(arr2));
+
+
+
+//динамічна функція
+// let averageSum =new Function ('array','  let newArray = []; let sum = 0; for (let elem of arr) {   if (typeof elem === "number") {     newArray.push(elem);     sum += elem;   } } return sum / newArray.length;')
+//  const arr = ["hello", 1, 7, 9, "c", "apple", 6, false, true, 44.8];
+// console.log(averageSum(arr));
