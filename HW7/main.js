11057b66879c319d1d89c8bcd49ff493e967fb00
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-01-30 17:46:32 +0200

    hw7

---
+++ HW7/main.js
@@ -0,0 +1,53 @@
+let company = {
+    sales: [
+        {
+            name: 'John',
+            salary: 1000
+        },
+        {
+            name: 'Alice',
+            salary: 600
+        }
+    ],
+    development: {
+        web: [
+            {
+                name: 'Peter',
+                salary: 2000
+            },
+            {
+                name: 'Alex',
+                salary: 1800
+            }
+        ],
+        internals: [
+            {
+                name: 'Jack',
+                salary: 1300
+            }
+        ]
+    }
+}
+
+function countSalary(company){
+    let sum=0;
+    for(let key in company){
+        if(Array.isArray(company[key])){
+            for(let elem of company[key]){
+                sum+=elem.salary
+            }
+        }
+        else if(typeof(company[key])==='object' && company[key]!==null){
+            sum+=countSalary(company[key])
+        }
+    }
+    return sum
+}
+console.log(countSalary(company))
+
+
+
+
+
+
+
