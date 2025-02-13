bc3660530ea8c590aed332ecf61e320f842faac6
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-02-13 15:46:17 +0200

    12/3

--- HW12/t12.3/main.js
+++ HW12/t12.3/main.js
@@ -8,12 +8,7 @@ addBtn.addEventListener('click', () => {
         li.textContent = input.value;
         let deleteBtn = document.createElement('button');
 
-        deleteBtn.style.marginLeft = '10px'
-        deleteBtn.style.marginBottom = '5px'
-        deleteBtn.style.padding = '10px'
-        deleteBtn.style.color = 'white'
-        deleteBtn.style.backgroundColor = 'red'
-        deleteBtn.style.border = 'none'
+       deleteBtn.classList.add('style-btn')
 
         deleteBtn.textContent = 'Видалити елемент';
         li.appendChild(deleteBtn)
