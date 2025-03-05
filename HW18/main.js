a6df2cc90a25057f56ea3b9be889fb524a75e3d8
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-03-05 19:30:52 +0200

    timer

--- HW18/main.js
+++ HW18/main.js
@@ -1,38 +1,32 @@
-// Реалізувати таймер відліку:
 
-// Початок таймера визначати із змінної
-// Відобразити на сторінці час у форматі 01:25
-// Коли закінчився таймер зупинити його
-
-// let timerText = document.getElementsByTagName('h1');
 let minutesTimer = document.querySelector('.minutes')
 let secondsTimer = document.querySelector('.seconds');
 
-    let minute = parseInt(minutesTimer.textContent);
-    let seconds = parseInt(secondsTimer.textContent)
+let totalTime = 85;
+function updateTimer(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    minutesTimer.textContent = minutes < 10 ? '0' + minutes : minutes;
+    secondsTimer.textContent = seconds < 10 ? '0' + seconds : seconds;
+
+}
+
 
 const countDown = setInterval(() => {
 
-
-
-    if (minute === 0 && seconds === 0) {
+    if (totalTime<=0) {
         clearInterval(countDown)
         let stoppedInterval = document.createElement('h5');
         stoppedInterval.textContent = 'Таймер завершено';
         stoppedInterval.classList.add('stop--interval')
-        document.body.appendChild(stoppedInterval)
+        document.body.appendChild(stoppedInterval);
         return;
 
     }
-    if (seconds > 0) {
-        seconds--;
-    }
-    else if (minute > 0) {
-        minute--;
-        seconds = 59
-    }
+   totalTime--;
+   updateTimer(totalTime)
 
-
-    minutesTimer.textContent = minute < 10 ? "0" + minute : minute;
-    secondsTimer.textContent = seconds < 10 ? "0" + seconds : seconds;
 }, 1000);
+
+updateTimer(totalTime)
