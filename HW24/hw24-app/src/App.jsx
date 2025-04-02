ba21d78647cebf50f2aef0222e69f48e17c1b573
Khrystyna-Hnatiuk <gnatiukkris@gmail.com>
2025-04-02 19:35:02 +0300

    changed css

--- HW24/hw24-app/src/App.jsx
+++ HW24/hw24-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import lukeImg from './assets/star.jpg'
 import starsImg from './assets/star-svgrepo-com.svg'
